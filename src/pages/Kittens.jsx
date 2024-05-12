6a8b34dfd3169e6f204402327912292294deccb7
Josue Morales <157775085+PR2014FL@users.noreply.github.com>
2024-05-12 12:59:23 -0400

    Addes PageTitle component to all pages and added some styling.

--- src/pages/Kittens.jsx
+++ src/pages/Kittens.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PageImage from '../components/PageImage'
+import PageTitle from '../components/PageTitle'
 
 export default function Kittens() {
 
 
   return (
     <div className='kittensDiv'>
-        <h1 className="kittensH1">This is the Kittens Page</h1>
+        <PageTitle content="This is the Kittens Page"/>
         <PageImage src="https://images.pexels.com/photos/3073694/pexels-photo-3073694.jpeg?auto=compress&cs=tinysrgb&w=600" alt="two white kittens" />
         </div>
   )
--- src/pages/Puppies.jsx
+++ src/pages/Puppies.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import PageImage from '../components/PageImage'
+import PageTitle from '../components/PageTitle'
 
 function Puppies() {
   return (
     <div className='puppiesDiv'>
-        <h1 className='puppiesH1'>This is the Puppies Page</h1>
+        <PageTitle content="This is the Puppies Page"/>
         <PageImage src="https://images.pexels.com/photos/7324407/pexels-photo-7324407.jpeg?auto=compress&cs=tinysrgb&w=600" alt="11 golden retriever pups in the back of an old chevrolet pickup truck"  />
         </div>
   )
